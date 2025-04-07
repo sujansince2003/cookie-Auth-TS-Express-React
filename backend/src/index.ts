f251de35d7d66b9aba4a53a96e4924cbfa0524bd
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-07 23:59:32 +0545

    setup cookie based auth checks in backend

---
+++ backend/src/index.ts
@@ -0,0 +1,45 @@
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import path from "path";
+const jwtsecret = "myjwtsecret"
+const app = express();
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:5173"
+}))
+
+
+app.post("/signin", (req, res) => {
+    const token = jwt.sign({ username: "sujan", id: 1 }, jwtsecret)
+    res.cookie("token", token)  //cookie parser ::: send set-cookie value to client
+    res.json({ msg: "sign in successfully" })
+})
+
+app.get("/user", (req, res) => {
+    const cookieToken = req.cookies.token;
+
+    const decoded = jwt.verify(cookieToken, jwtsecret) as JwtPayload;
+
+    const { username, id } = decoded;
+    res.json({ username, id })
+
+})
+
+app.post("/logout", (req, res) => {
+    res.clearCookie("token");
+    res.json({ msg: "logged out" })
+})
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("server running on port 3000")
+})
